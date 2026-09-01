b65b802bdcd879392756ba7948e70a12746d5404
Neto Rodrigues <netobili22@gmail.com>
2026-09-01 20:58:35 -0300

    Adicionando subtração

--- calculadora.py
+++ calculadora.py
@@ -2,6 +2,10 @@ n1 = float(input('Digite um numero: '))
 n2 = float(input('Digite o segunbdo numero: '))
 
 r = n1 + n2
+s = n1 - n2
 
-print(f'Resultado = {r}')
+
+print(f'Adição, Resultado = {r}')
+print('='*30)
+print(f'Subtração, Resultado {s}')
 
